e09909dff4aad94cb272bed6eb484b9cbca06810
ramya72002 <nsriramya7@gmail.com>
2024-11-20 19:06:02 +0530

    mesag changed to support

--- src/container/services/index.jsx
+++ src/container/services/index.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
+import { Container, Row, Col } from "react-bootstrap";
+import ServicesItem from "../../components/services";
 import { ServicesSection } from "./style";
 const ServicsArea = ({ data }) => {
     return (
         <ServicesSection>
-            {/* <Container>
+            <Container>
                 <Row className="gx-5">
                     {data?.items &&
                         data?.items.map((item, i) => {
@@ -22,7 +24,7 @@ const ServicsArea = ({ data }) => {
                             );
                         })}
                 </Row>
-            </Container> */}
+            </Container>
         </ServicesSection>
     );
 };
