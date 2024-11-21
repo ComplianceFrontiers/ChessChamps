1efa4bf5bd79402952967990077e077d09921f26
ramya72002 <nsriramya7@gmail.com>
2024-11-21 10:15:31 +0530

    images centeralign

--- src/container/home/brand/style.jsx
+++ src/container/home/brand/style.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import styled, { device, themeGet } from "@theme/utils";
 import BrandBg from "../../../data/images/brand-logo/bg.png";
 export const BrandSection = styled.section``;
