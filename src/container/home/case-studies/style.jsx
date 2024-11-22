f23e54579d42a837c7484676b37e2414e4a4c3ef
ramya72002 <nsriramya7@gmail.com>
2024-11-22 16:23:12 +0530

    icons added

--- src/container/home/case-studies/style.jsx
+++ src/container/home/case-studies/style.jsx
@@ -1,56 +1,32 @@
+/* eslint-disable prettier/prettier */
 import styled, { device } from "@theme/utils";
 export const CaseStudiesSection = styled.section`
-    .swiper-slide-active {
-        .content-text {
-            display: block;
-        }
+    .button-wrapper {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px; // Adjust as needed
     }
-    .case-studies-slider-button {
-        position: relative;
-        z-index: 3;
-        ${device.large} {
-            max-width: 1040px;
-            margin: auto;
-            padding: 0 20px;
-        }
+
+    .shop-now-button {
+    margin-bottom: 30px;
+    background-color: #fc7418; /* Button background color */
+    color: #fff; /* Text color */
+    font-size: 16px; /* Text size */
+    width: 200px; /* Explicit width */
+    height: 60px; /* Explicit height */
+    padding: 10px 20px; /* Adjust padding for internal spacing */
+    border: none; /* No border */
+    border-radius: 10px; /* Slightly more rounded corners */
+    cursor: pointer; /* Pointer cursor on hover */
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Button shadow for depth */
+    transition: background-color 0.3s ease, transform 0.2s ease; /* Smooth hover effect */
+
+    &:hover {
+        background-color: #feb423; /* Lighter background on hover */
+        transform: scale(1.05); /* Slightly enlarge button on hover */
     }
-    .case-studies-slider-button-prev,
-    .case-studies-slider-button-next {
-        background: linear-gradient(
-            to right,
-            #fc7318,
-            #fe8415,
-            #fe9515,
-            #fea51a,
-            #feb423
-        );
-        border: solid 2px transparent;
-        background-image: linear-gradient(
-                rgba(255, 255, 255, 0),
-                rgba(255, 255, 255, 0)
-            ),
-            linear-gradient(101deg, #fc7318 0%, #feb423 100%);
-        background-origin: border-box;
-        background-clip: content-box, border-box;
-        -webkit-box-shadow: 2px 1000px 1px #f7faff inset;
-        box-shadow: 2px 1000px 1px #f7faff inset;
-        color: #fc7418;
-        width: 55px;
-        height: 55px;
-        line-height: 55px;
-        text-align: center;
-        color: #fff;
-        font-size: 20px;
-        border-radius: 50%;
-        position: absolute;
-        top: -150px;
-        i {
-            color: #fc7418;
-        }
-    }
-    .case-studies-slider-button-next {
-        right: 10px;
-        left: auto;
-    }
+}
+
 `;
+
 export const CaseStudiesItemWrap = styled.div``;
