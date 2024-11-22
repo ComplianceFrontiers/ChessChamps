4e10aa1fb31b72ca44ceaa6baaf1547f94b60334
ramya72002 <nsriramya7@gmail.com>
2024-11-22 11:23:26 +0530

    icons added

--- src/container/home/team/style.jsx
+++ src/container/home/team/style.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import styled, { device } from "@theme/utils";
 import BeforeImge from "../../../data/images/team/shape-bottom.png";
 import AfterImge from "../../../data/images/team/shape-top.png";
