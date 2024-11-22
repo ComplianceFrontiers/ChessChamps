87a84970a1f2978872b01c421ac1d210a2b62058
ramya72002 <nsriramya7@gmail.com>
2024-11-22 16:40:39 +0530

    icons added

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -1,22 +1,18 @@
+/* eslint-disable prettier/prettier */
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import PropTypes from "prop-types";
 import { BrandSingleItem, BrandItem } from "./style";
-const SingleBrand = ({ brnadAffterImage, brnadBeforeImage, alt }) => {
+const SingleBrand = ({ brnadBeforeImage, alt }) => {
     const image1 = getImage(brnadAffterImage);
-    const image2 = getImage(brnadBeforeImage);
-    return (
+     return (
         <BrandSingleItem href="/">
             <BrandItem className="brandBefore">
                 <GatsbyImage image={image1} alt={alt} />
-            </BrandItem>
-            <BrandItem className="brandAfter">
-                <GatsbyImage image={image2} alt={alt} />
             </BrandItem>
         </BrandSingleItem>
     );
 };
 SingleBrand.propTypes = {
-    brnadAffterImage: PropTypes.object,
     brnadBeforeImage: PropTypes.object,
     alt: PropTypes.string,
 };
--- src/container/home/working-process/style.jsx
+++ src/container/home/working-process/style.jsx
@@ -18,76 +18,25 @@ export const WorkingProcessSection = styled.section`
     background-size: cover;
     background-repeat: no-repeat;
     padding: 130px 0px 115px;
+
+    .image-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px; /* Space between images */
+        flex-wrap: wrap; /* Allows images to wrap on smaller screens */
+    }
+
+    .image-wrapper {
+        max-width: 150px; /* Adjust size as needed */
+        text-align: center;
+    }
+
     .working-process {
         .working-process-list {
             margin-bottom: 30px;
             position: relative;
             text-align: center;
-            &:nth-of-type(odd) {
-                margin-top: 0px;
-                ${device.large} {
-                    margin-top: 100px;
-                }
-                ${device.xlarge} {
-                    margin-top: 125px;
-                }
-            }
-            &:nth-of-type(even) {
-                .arrow-shape {
-                    display: none !important;
-                    ${device.large} {
-                        display: block !important;
-                        top: 100px;
-                    }
-                    ${device.xlarge} {
-                        top: 125px;
-                    }
-                }
-            }
-            &:last-child {
-                .arrow-shape {
-                    display: none !important;
-                }
-            }
-            &:hover {
-                .icon {
-                    &::before {
-                        position: absolute;
-                        left: -8px;
-                        top: -8px;
-                        content: "";
-                        background-image: url(${circleImage});
-                        width: 110%;
-                        height: 110%;
-                        background-position: center center;
-                        background-size: cover;
-                        background-repeat: no-repeat;
-                        z-index: 9;
-                        opacity: 0;
-                        visibility: hidden;
-                    }
-                    &::before {
-                        animation: ${rotation} 10s infinite linear;
-                        opacity: 1;
-                        visibility: visible;
-                    }
-                }
-            }
-        }
-    }
-    .arrow-shape {
-        position: absolute !important;
-        top: 60px;
-        right: -30px;
-        display: none !important;
-        ${device.small} {
-            display: block !important;
-        }
-        ${device.large} {
-            top: -10px;
-        }
-        ${device.xlarge} {
-            top: -10px;
         }
     }
 `;
