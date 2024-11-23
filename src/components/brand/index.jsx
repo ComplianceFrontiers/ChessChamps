947b50b9213513a071d10be8b88be6147115e4a5
ramya72002 <nsriramya7@gmail.com>
2024-11-23 22:05:34 +0530

    icons added

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -1,17 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import PropTypes from "prop-types";
-import { BrandSingleItem, BrandItem } from "./style";
-const SingleBrand = ({ brnadAffterImage, brnadBeforeImage, alt }) => {
+import { BrandSingleItem} from "./style";
+const SingleBrand = ({ brnadAffterImage, alt }) => {
     const image1 = getImage(brnadAffterImage);
-    const image2 = getImage(brnadBeforeImage);
     return (
         <BrandSingleItem href="/">
-            <BrandItem className="brandBefore">
                 <GatsbyImage image={image1} alt={alt} />
-            </BrandItem>
-            <BrandItem className="brandAfter">
-                <GatsbyImage image={image2} alt={alt} />
-            </BrandItem>
         </BrandSingleItem>
     );
 };
