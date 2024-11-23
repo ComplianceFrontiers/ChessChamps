7279e08849ee8b336b9bb8cbda384c5d09cbbc09
ramya72002 <nsriramya7@gmail.com>
2024-11-23 21:30:54 +0530

    icons added

--- src/container/home/team/style.jsx
+++ src/container/home/team/style.jsx
@@ -3,15 +3,15 @@ import styled, { device } from "@theme/utils";
 import BeforeImge from "../../../data/images/team/shape-bottom.png";
 import AfterImge from "../../../data/images/team/shape-top.png";
 export const TeamSection = styled.section`
-    padding: 100px 0px;
+    padding: 50px 0px;
     ${device.medium} {
-        padding: 190px 0px;
+        padding: 150px 0px;
     }
     ${device.large} {
-        padding: 190px 0px;
+        padding: 150px 0px;
     }
     ${device.xlarge} {
-        padding: 290px 0px;
+        padding: 150px 0px;
     }
     position: relative;
     background-image: linear-gradient(
@@ -43,18 +43,5 @@ export const TeamSection = styled.section`
         background-repeat: no-repeat;
         background-size: cover;
         padding-bottom: 11%;
-    }
-    &::after {
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 0;
-        content: "";
-        background-image: url(${AfterImge});
-        background-position: left center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        padding-top: 11%;
     }
 `;
