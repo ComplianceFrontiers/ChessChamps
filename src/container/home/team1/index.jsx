80490dc4f97c56a2e204fad59bbdcbf4c2fe363b
ramya72002 <nsriramya7@gmail.com>
2024-11-25 12:27:46 +0530

    icons added

--- src/container/home/team1/index.jsx
+++ src/container/home/team1/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import PropTypes from "prop-types";
 import { StaticImage } from "gatsby-plugin-image";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap"; // Import Button from Bootstrap
 import Swiper, { SwiperSlide } from "@components/shared/swiper"; // Ensure Swiper is imported
 import TeamMember from "../../../components/team";
 import SectionTitle from "../../../components/title";
@@ -20,49 +20,22 @@ const TeamArea1 = ({ data }) => {
                     <SectionTitle
                         headingTitle={
                             <div>
-                                <span style={{ fontSize: "0.5em" }}>demo</span>
+                                <span style={{ fontSize: "0.8em" }}>
+                                    Chess is more than a game; it’s a mental fitness regime, just like any sport or physical
+                                    fitness program.
+                                </span>
                                 <br />
-                                <span style={{ fontSize: "0.8em" }}>Delaware Junior Chess Team</span>
+                                <span style={{ fontSize: "0.5em" }}>
+                                    Chess is more than just a game; it’s a mental fitness regime, strengthening focus, strategy, and
+                                    decision-making skills. Just like physical training shapes the body, chess hones the mind for
+                                    life’s challenges.
+                                </span>
                             </div>
                         }
                         {...data.section_title}
                     />
                 )}
-                <Swiper
-                    layout={{
-                        nav: "team-navigation",
-                        dots: "team-dots-style",
-                    }}
-                    navigation={{
-                        nextEl: ".team-slider-button-next",
-                        prevEl: ".team-slider-button-prev",
-                    }}
-                    slidesPerView={4}
-                    spaceBetween={30}
-                    autoplay={{
-                        delay: 1000,
-                        disableOnInteraction: false,
-                    }}
-                    breakpoints={{
-                        320: { slidesPerView: 1 },
-                        480: { slidesPerView: 2 },
-                        768: { slidesPerView: 3 },
-                        992: { slidesPerView: 4 },
-                    }}
-                >
-                    {data?.items &&
-                        data.items.map((item, i) => (
-                            <SwiperSlide key={i}>
-                                <TeamMember
-                                    title={item.headings?.[0]?.content || "Default Title"}
-                                    level={item.headings?.[0]?.level || ""}
-                                    teamImage={item.images?.[0]?.src || ""}
-                                    designation={item.designation || "Team Member"}
-                                    socials={item.socials || []}
-                                />
-                            </SwiperSlide>
-                        ))}
-                </Swiper>
+              
             </Container>
         </TeamSection>
     );
