f050266171e7048d1176bc2d7f9ea14cfb3cb861
TanujKumar41 <tanujkumar191001@gmail.com>
2024-11-28 19:55:20 +0530

    changes

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types";
 import { Accordion } from "react-bootstrap";
 import { FaqListItem } from "./style";
-const FaqList = ({ numberItem, title, description }) => {
+import { StaticImage } from "gatsby-plugin-image";
+
+const FaqList = ({ numberItem, title, imageSrc, description }) => {
+    console.log(title, imageSrc);
+
     return (
         <FaqListItem>
             <Accordion.Item eventKey={`${numberItem}`}>
                 <Accordion.Header>
+                    {/* Display the image using StaticImage */}
+                    <StaticImage
+                        src={imageSrc} // The path to the static image
+                        alt={`Image for ${title}`}
+                        style={{ width: "50px", height: "50px", marginRight: "10px" }}
+                    />
                     <span>
                         {numberItem}. {title}
                     </span>
@@ -17,9 +27,12 @@ const FaqList = ({ numberItem, title, description }) => {
         </FaqListItem>
     );
 };
+
 FaqList.propTypes = {
     title: PropTypes.string,
     numberItem: PropTypes.number,
     description: PropTypes.string,
+    imageSrc: PropTypes.string.isRequired, // Ensure it's a string path for StaticImage
 };
+
 export default FaqList;
