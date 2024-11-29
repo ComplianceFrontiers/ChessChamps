88c3fc345d1ec4d66076e9e1d062db4485e34e11
ramya72002 <nsriramya7@gmail.com>
2024-11-29 11:26:14 +0530

    logo changed for small devices

--- src/container/home/team1/style.jsx
+++ src/container/home/team1/style.jsx
@@ -3,6 +3,7 @@ import styled, { device } from "@theme/utils";
 import BeforeImge from "../../../data/images/team/shape-bottom.png";
 import AfterImge from "../../../data/images/team/shape-top.png";
 export const TeamSection = styled.section`
+margin-top:100px;
     padding: 50px 0px;
     ${device.medium} {
         padding: 150px 0px;
@@ -25,12 +26,24 @@ export const TeamSection = styled.section`
     background-position: left center;
     background-repeat: no-repeat;
     background-size: cover;
+
     .pattern {
         position: absolute !important;
-        top: 0;
-        left: 0;
-        width: 100%;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: auto; /* Maintain the natural width */
+        height: auto; /* Maintain the natural height */
+       
+
+    @media (max-width: 480px) { /* For even smaller devices */
+    transform: translate(-50%, -50%);    
+    top: -10%;
+    width: 20%; /* Adjust width as needed */
+        height: 50%; /* Maintain aspect ratio */
     }
+    }
+
     &::before {
         position: absolute;
         left: 0;
@@ -44,27 +57,30 @@ export const TeamSection = styled.section`
         background-size: cover;
         padding-bottom: 11%;
     }
-        .button-wrapper {
+
+    .button-wrapper {
         display: flex;
         justify-content: center;
-        margin-top: 20px; // Adjust as needed
+        margin-top: 20px;
     }
 
     .shop-now-button {
-    background-color: #9f8460; /* Button background color */
-    color: #fff; /* Text color */
-    font-size: 16px; /* Text size */
-    width: 160px; /* Explicit width */
-    height: 50px; /* Explicit height */
-    padding: 10px 20px; /* Adjust padding for internal spacing */
-    border: none; /* No border */
-    border-radius: 10px; /* Slightly more rounded corners */
-    cursor: pointer; /* Pointer cursor on hover */
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Button shadow for depth */
-    transition: background-color 0.3s ease, transform 0.2s ease; /* Smooth hover effect */
+        background-color: #9f8460;
+        color: #fff;
+        font-size: 16px;
+        width: 160px;
+        height: 50px;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        transition: background-color 0.3s ease, transform 0.2s ease;
 
-    &:hover {
-        background-color: #feb423; /* Lighter background on hover */
-        transform: scale(1.05); /* Slightly enlarge button on hover */
+        &:hover {
+            background-color: #feb423;
+            transform: scale(1.05);
+        }
     }
 `;
+
