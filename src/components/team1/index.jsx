4bc7d306094b88050b95728846d72663bfccf145
ramya72002 <nsriramya7@gmail.com>
2024-12-01 16:04:41 +0530

    logo changed for small devices

--- src/components/team1/index.jsx
+++ src/components/team1/index.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable prettier/prettier */
 import PropTypes from "prop-types";
-import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 import {
     SingleTeam,
     Thumb,
@@ -10,31 +10,56 @@ import {
     TeamSocialItem,
     TeamSocialLink,
 } from "./style";
-const TeamMember = ({ teamImage, title, designation, socials, level }) => {
-    const image1 = getImage(teamImage);
+
+// Import images
+import image1 from "../../data/images/blog/chessclubgallery/image1.png";
+import image2 from "../../data/images/blog/chessclubgallery/image2.png";
+import image3 from "../../data/images/blog/chessclubgallery/image3.png";
+import image4 from "../../data/images/blog/chessclubgallery/image4.png";
+
+const TeamMember = ({ title, designation, socials, level }) => {
+    // Determine the image based on the title
+    let teamImage;
+
+    switch (title) {
+        case "Thanvish Reddy":
+            teamImage = image1;
+            break;
+        case "Bill Truemam":
+            teamImage = image2;
+            break;
+        case "Jatin Joshi":
+            teamImage = image3;
+            break;
+        case "Siddharth Bose":
+            teamImage = image4;
+            break;
+        default:
+            teamImage = image1;  // Fallback to image1 if no match
+    }
+
     return (
         <SingleTeam>
             <Thumb>
-                <GatsbyImage image={image1} alt={title} />
+                {/* Use the <img> tag for the team image */}
+                <img src={teamImage} alt={title} />
                 <div className="social-hover">
-                    <StaticImage
+                    {/* Use the <img> tag for the hover image */}
+                    <img
                         src="../../data/images/team/team-hover.png"
                         alt={title}
                     />
                 </div>
             </Thumb>
-            <TeamContent>
-                <Title as={level}>{title}</Title>
-                <Designation>{designation}</Designation>
-            </TeamContent>
         </SingleTeam>
     );
 };
+
 TeamMember.propTypes = {
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
     level: PropTypes.string,
-    teamImage: PropTypes.object,
     designation: PropTypes.string,
     socials: PropTypes.array,
 };
+
 export default TeamMember;
