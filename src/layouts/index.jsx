fd3e0807bb0069712ae894cf20f957c886cec026
ramya72002 <nsriramya7@gmail.com>
2024-12-23 10:29:16 +0530

    removed footer for events

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import PropTypes from "prop-types";
 import theme from "@theme";
 import { ThemeProvider } from "theme-ui";
@@ -7,15 +8,19 @@ import "@assets/css/icofont.min.css";
 import Header from "./header";
 import Footer from "./footer";
 import ScrollToTop from "../components/shared/scroll-to-top";
+import { useLocation } from "@reach/router"; // Gatsby-specific hook
 
 const Layout = ({ data, children }) => {
+    const location = useLocation(); // Get current location
+
     return (
         <ThemeProvider theme={theme}>
             <div className="wrapper">
                 <GlobalCSS />
                 <Header data={data} />
                 {children}
-                <Footer data={data} />
+                {/* Hide Footer on the /events and /newjersy/ pages */}
+                {(location.pathname !== "/events" && location.pathname !== "/events"  && location.pathname !== "/newjersy/" && location.pathname !== "/newjersy" )&& <Footer data={data} />}
                 <ScrollToTop />
             </div>
         </ThemeProvider>
