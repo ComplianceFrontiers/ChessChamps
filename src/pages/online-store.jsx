b32b8a45c59961c6768755ea1b95ddf18ac6f593
ramya72002 <nsriramya7@gmail.com>
2024-12-24 14:47:56 +0530

    stripe url added

--- src/pages/online-store.jsx
+++ src/pages/online-store.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import PropTypes from "prop-types";
 import Layout from "@layout";
 import SEO from "@components/seo";
+import { Link } from "gatsby"; // Importing Gatsby's Link component
 import { graphql } from "gatsby";
 import PageBreadcrumb from "../components/pagebreadcrumb";
 import { normalizedData } from "@utils/functions";
@@ -23,6 +27,28 @@ const FAQPage = ({ data, location, pageContext }) => {
                 location={location}
                 title="Online Store"
             />
+             {/* <Link to="/online-class-sell" >
+            <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "20px",
+            }}
+       
+        >
+    <img
+        src={image1}
+        alt="FAQ Illustration"
+        style={{
+            maxWidth: "auto",
+            height: "auto",
+            borderRadius: "8px",
+            cursor: "pointer", // Add a pointer cursor for better UX
+        }}
+    />
+</div>
+</Link> */}
+
             <div
             style={{
                 display: "flex",
