9abb2706fca19fc887e5d62926d7d08db69f2f25
ramya72002 <nsriramya7@gmail.com>
2024-12-25 23:11:54 +0530

    stripe url added

---
+++ src/pages/NJCC_Gallery.jsx
@@ -0,0 +1,133 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable prettier/prettier */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import React from "react";
+import PropTypes from "prop-types";
+import Layout from "@layout";
+import SEO from "@components/seo";
+import { graphql } from "gatsby";
+import PageBreadcrumb from "../components/pagebreadcrumb";
+import { normalizedData } from "@utils/functions";
+
+// Import images for 9.png to 15.png
+import image9 from "../data/images/celebrating-nationals-win/16.png";
+import image10 from "../data/images/celebrating-nationals-win/17.png";
+import image11 from "../data/images/celebrating-nationals-win/18.png";
+import image12 from "../data/images/celebrating-nationals-win/19.png";
+import image13 from "../data/images/celebrating-nationals-win/20.png";
+
+const FAQPage = ({ data, location, pageContext }) => {
+    const globalContent = normalizedData(data?.allGeneral?.nodes || []);
+    const content = normalizedData(data?.page.content || []);
+
+    return (
+        <Layout
+            data={{
+                ...globalContent["menu"],
+                ...globalContent["footer"],
+            }}
+        >
+            <SEO title="NJCC Gallery" pathname="/" />
+            <PageBreadcrumb
+                pageContext={pageContext}
+                location={location}
+                title="NJCC Gallery"
+            />
+            <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap", // Allows images to wrap to the next line
+                    justifyContent: "center",
+                    gap: "20px", // Space between images
+                }}
+            >
+                {/* Display each image one after the other */}
+                <img
+                    src={image9}
+                    alt="Image 9"
+                    style={{
+                        maxWidth: "90%",
+                        marginBottom: "20px", // Space between images
+                    }}
+                />
+                <img
+                    src={image10}
+                    alt="Image 10"
+                    style={{
+                        maxWidth: "90%",
+                        marginBottom: "20px",
+                    }}
+                />
+                <img
+                    src={image11}
+                    alt="Image 11"
+                    style={{
+                        maxWidth: "90%",
+                        marginBottom: "20px",
+                    }}
+                />
+                <img
+                    src={image12}
+                    alt="Image 12"
+                    style={{
+                        maxWidth: "90%",
+                        marginBottom: "20px",
+                    }}
+                />
+                <img
+                    src={image13}
+                    alt="Image 13"
+                    style={{
+                        maxWidth: "90%",
+                        marginBottom: "20px",
+                    }}
+                />
+
+            </div>
+        </Layout>
+    );
+};
+
+FAQPage.propTypes = {
+    location: PropTypes.object,
+    pageContext: PropTypes.object,
+    data: PropTypes.shape({
+        allGeneral: PropTypes.shape({
+            nodes: PropTypes.arrayOf(PropTypes.shape({})),
+        }),
+        page: PropTypes.shape({
+            content: PropTypes.arrayOf(PropTypes.shape({})),
+        }),
+    }),
+};
+
+export const query = graphql`
+    query FAQPageQuery {
+        allGeneral {
+            nodes {
+                section
+                id
+                menu {
+                    ...Menu
+                }
+                footer {
+                    ...Footer
+                }
+            }
+        }
+        page(title: { eq: "FAQPage" }, pageType: { eq: innerpage }) {
+            content {
+                ...PageContentAll
+                section_title {
+                    ...SectionTitle
+                }
+            }
+        }
+    }
+`;
+
+export default FAQPage;
