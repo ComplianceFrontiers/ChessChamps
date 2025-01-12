5cd744d02de0a7db7391340c90ecbfa303f69027
ramya72002 <nsriramya7@gmail.com>
2025-01-12 17:50:00 +0530

    done

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,10 +1,9 @@
-/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Layout from "@layout";
 import SEO from "@components/seo";
 import PageBreadcrumb from "../components/pagebreadcrumb";
-import { graphql, Link } from "gatsby"; // Importing Link from Gatsby
+import { graphql, Link } from "gatsby";
 import { normalizedData } from "@utils/functions";
 import image2 from "../data/images/online/image3.png";
 import image3 from "../data/images/online/image4.png";
@@ -17,7 +16,15 @@ import image6 from "../data/images/online/image114.png";
 const FAQPage = ({ data, location, pageContext }) => {
     const globalContent = normalizedData(data?.allGeneral?.nodes || []);
     const content = normalizedData(data?.page.content || []);
-    const [isHovered, setIsHovered] = useState(false);
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+    const handleThirdImageClick = () => {
+        setIsPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupVisible(false);
+    };
 
     return (
         <Layout
@@ -67,31 +74,25 @@ const FAQPage = ({ data, location, pageContext }) => {
                         className="responsive-image"
                     />
                 </Link>
-                <Link to="/Middletown">
+
+                <div>
                     <img
+                        onClick={handleThirdImageClick}
                         src={image6}
-                        alt="FAQ Illustration 2"
+                        alt="FAQ Illustration 3"
                         style={{
                             width: "1000px", // Default for mobile
                             height: "auto",
                             borderRadius: "8px",
+                            cursor: "pointer"
                         }}
                         className="responsive-image"
-                    />
-                </Link>
-                {/* <Link to="/delaware-chess-champs">
-                    <img
-                        src={image9}
-                        alt="FAQ Illustration 2"
-                        style={{
-                            width: "1000px", // Default for mobile
-                            height: "auto",
-                            borderRadius: "8px",
-                        }}
-                        className="responsive-image"
-                    />
-                </Link> */}
-                <Link to="/DCC-Tournament">
+                     />
+                </div>
+                
+
+                
+                {/* <Link to="/DCC-Tournament">
                     <img
                         src={image1}
                         alt="FAQ Illustration 2"
@@ -102,7 +103,7 @@ const FAQPage = ({ data, location, pageContext }) => {
                         }}
                         className="responsive-image"
                     />
-                </Link>
+                </Link> */}
                 <Link to="/NJCC">
                     <img
                         src={image3}
@@ -115,6 +116,7 @@ const FAQPage = ({ data, location, pageContext }) => {
                         className="responsive-image"
                     />
                 </Link>
+
                 <Link to="https://chess-champs-tournaments.vercel.app/">
                     <img
                         src={image2}
@@ -128,32 +130,66 @@ const FAQPage = ({ data, location, pageContext }) => {
                     />
                 </Link>
             </div>
+
+            {isPopupVisible && (
+                <div
+                    style={{
+                        position: "fixed",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        backgroundColor: "white",
+                        padding: "20px",
+                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                        zIndex: 1000,
+                        textAlign: "center",
+                        borderRadius: "8px",
+                    }}
+                >
+                    <p>Launching Soon</p>
+                    <button
+                        style={{
+                            marginTop: "10px",
+                            padding: "5px 10px",
+                            backgroundColor: "#007BFF",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "5px",
+                            cursor: "pointer",
+                        }}
+                        onClick={closePopup}
+                    >
+                        Close
+                    </button>
+                </div>
+            )}
+
             <style>
                 {`
-    .responsive-image {
-        transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
-    }
-    @media (max-width: 768px) {
-        .responsive-image {
+                .responsive-image {
+                    transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
+                }
+                @media (max-width: 768px) {
+                    .responsive-image {
             width: 100%; /* Adjusted for smaller screens */
             height: auto;
             padding-left:10px;
              padding-right:10px;
-        }
-    }
-    @media (min-width: 768px) {
-        .responsive-image {
+                    }
+                }
+                @media (min-width: 768px) {
+                    .responsive-image {
             width: 80%; /* Larger size for tablets */
             height: auto;
-        }
-    }
-    @media (min-width: 1024px) {
-        .responsive-image {
+                    }
+                }
+                @media (min-width: 1024px) {
+                    .responsive-image {
             width: 70%; /* Larger size for desktops */
             height: auto;
-        }
-    }
-    `}
+                    }
+                }
+                `}
             </style>
         </Layout>
     );
