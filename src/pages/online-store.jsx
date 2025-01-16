8371ed8a2b2aebdf84fa90cfe875317d748e692d
ramya72002 <nsriramya7@gmail.com>
2025-01-16 11:12:20 +0530

    done

--- src/pages/online-store.jsx
+++ src/pages/online-store.jsx
@@ -25,6 +25,7 @@ const FAQPage = ({ data, location, pageContext }) => {
         child_last_name: "",
         phone: "",
         BasicsOfChess_Online: true,
+        SchoolName:"online",
         onlinePurchase:false,
     });
 
