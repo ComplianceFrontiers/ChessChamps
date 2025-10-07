796cc17b88ee79b28806452fc7a32117a881b1c7
ramya <nsriramya7@gmailcom>
2025-10-07 23:42:30 +0530

    done

--- src/components/blog/blog-card-two/index.jsx
+++ src/components/blog/blog-card-two/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import { getImage, GatsbyImage } from "gatsby-plugin-image";
@@ -12,27 +13,53 @@ import {
 const BlogCardTwo = ({ date, title, quote_text, slug, thume_image }) => {
     const image = getImage(thume_image);
 
-    // Determine button text and path
+    // Determine if it's Chess Store
     const isChessStore = title === "Chess Store";
-    const buttonText = isChessStore ? "Shop Now" : "Read More";
-    const buttonPath = isChessStore ? "/online-store" : `/${slug}`;
+    const buttonText = isChessStore ? "Visit" : "Read More";
+    const buttonPath = isChessStore
+        ? "https://chesschamps.academy/"
+        : `/${slug}`;
 
     return (
         <BlogGridCard>
-            <Link to={`/${slug}`} className="blog-grid-thumb">
+            {/* Thumbnail */}
+            <Link
+                to={isChessStore ? "https://chesschamps.academy/" : `/${slug}`}
+                className="blog-grid-thumb"
+                target={isChessStore ? "_blank" : "_self"}
+                rel={isChessStore ? "noopener noreferrer" : undefined}
+            >
                 <GatsbyImage image={image} alt={title} />
             </Link>
-            <BlogGridContent>
-                <Title>
-                    <Link to={`/${slug}`}>{title}</Link>
-                </Title>
-                <Quote_text>{quote_text}</Quote_text>
-                <div style={{ textAlign: "center" }}>
-                    <Button size="medium" path={buttonPath} shape="rounded5">
+
+            {/* Show BlogGridContent only if not Chess Store */}
+            {!isChessStore && (
+                <BlogGridContent>
+                    <Title>
+                        <Link to={`/${slug}`}>{title}</Link>
+                    </Title>
+                    <Quote_text>{quote_text}</Quote_text>
+                    <div style={{ textAlign: "center" }}>
+                        <Button size="medium" path={buttonPath} shape="rounded5">
+                            {buttonText}
+                        </Button>
+                    </div>
+                </BlogGridContent>
+            )}
+
+            {/* For Chess Store, show only button */}
+            {isChessStore && (
+                <div style={{ textAlign: "center", marginTop: "15px" }}>
+                    <Button
+                        size="medium"
+                        path={buttonPath}
+                        shape="rounded5"
+                        target="_blank"
+                    >
                         {buttonText}
                     </Button>
                 </div>
-            </BlogGridContent>
+            )}
         </BlogGridCard>
     );
 };
